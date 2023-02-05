d34a21f2271b1188063e24d036d6ee1411e958ca
Abdallamohamud <abdallamohamudfarah@gmail.com>
2023-02-05 09:16:02 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'screens/home.dart';
+import 'screens/login_register.dart';
+void main() async{
+  
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp();
+runApp(MaterialApp(
+   debugShowCheckedModeBanner: false,
+  home: AuthScreen(),),);
+}
